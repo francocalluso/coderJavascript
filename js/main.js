a9ed6d8e755b71f8a1bab080c1e87dd4baa202bd
francocalluso <francocalluso@gmail.com>
2021-09-28 11:31:26 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+
+const BLANCO = " ";
+let nombreUsuario = prompt("Ingrese su nombre aquí")
+let apellidoUsuario = prompt("Ingrese su apellido")
+let nombreCompleto = nombreUsuario + BLANCO + apellidoUsuario
+alert (" :) Buenos días "+nombreCompleto)
+
